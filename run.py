6d26956d5973650e0db10f3aaaa4146e0c1618a9
Naman Garg <115886209+namangarg1902@users.noreply.github.com>
2024-01-26 01:28:36 +0530

    Configured port problem

--- run.py
+++ run.py
@@ -15,4 +15,4 @@ def index():
     return render_template('index.html' , jsonData1 = jsonData1 , jsonData2 = jsonData2)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
