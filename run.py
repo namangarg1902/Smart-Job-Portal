232028ba0e072c682df9702e6aab0569d3a5e8b4
namangarg1902 <115886209+namangarg1902@users.noreply.github.com>
2024-01-26 16:05:58 +0530

    Minor Changes in Frontend Part

--- run.py
+++ run.py
@@ -15,4 +15,4 @@ def index():
     return render_template('index.html' , jsonData1 = jsonData1 , jsonData2 = jsonData2)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
